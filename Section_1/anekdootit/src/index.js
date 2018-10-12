9da363881c4a18b3cd971065113aa73d70010c1b
Juuso <jueskeli@student.jyu.fi>
2018-10-13 02:19:43 +0300

    Signed-off-by: Juuso <jueskeli@student.jyu.fi>

--- Section_1/anekdootit/src/index.js
+++ Section_1/anekdootit/src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  uusi = () => {
+    return () => {
+        this.setState({
+           selected : (Math.random() * (5 - 0 + 1) ) << 0
+        })
+    }
+}
+  render() {
+    return (
+      <div>
+        <h1>Arvotaan anekdootti :</h1>
+        {this.props.anecdotes[this.state.selected]} <br></br>
+        <Button 
+                handleClick={this.uusi()}
+                text="Uusi anekdootti"
+            />
+      </div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
