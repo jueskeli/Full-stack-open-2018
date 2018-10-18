14ed47ed5aa8faccd0222ac8af1d4faa69458cbd
Juuso <jueskeli@student.jyu.fi>
2018-10-18 19:47:48 +0300

    tehty tehtävä 2.9

--- Section_2/puhelin_luettelo/src/App.js
+++ Section_2/puhelin_luettelo/src/App.js
@@ -6,9 +6,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
-      newName: 'anna uusi nimi'
+      newName: 'anna uusi nimi',
+      newNumber: '000-00000000',
+      showOnly: ''
     }
   }
 
@@ -16,34 +21,54 @@ class App extends React.Component {
     event.preventDefault()
     console.log('nappia painettu')
        const bookObject ={
-         name: this.state.newName
+         name: this.state.newName,
+         number: this.state.newNumber
        }
-    if(this.state.persons.some(person => person.name === this.state.newName) === false){
+    if(this.state.persons.some(person => person.name === this.state.newName) === false &&
+       this.state.persons.some(person => person.number === this.state.newNumber) === false){
         const persons = this.state.persons.concat(bookObject)  
         this.setState({
            persons : persons,
-           newName : 'anna uusi nimi'
+           newName : 'anna uusi nimi',
+           newNumber : '000-0000000'
          })
     }
-    else alert('NIMI LÖYTYY JO OSOITEKIRJASTA')
+    else alert('NIMI TAI NUMERO LÖYTYY JO OSOITEKIRJASTA')
   }
 
-  handleChange = (event) => {
+  handleNameChange = (event) => {
     console.log(event.target.value)
     this.setState({newName : event.target.value})
   }
 
-  showNames = (persons) => {
-    
+  handleNumberChange = (event) => {
+    console.log(event.target.value)
+    this.setState({newNumber : event.target.value})
+  }
+
+  handleFilter = (event) => {
+    console.log(event.target.value)
+    this.setState({showOnly : event.target.value})
   }
 
   render() {
+    const namesToShow =
+      this.state.persons.filter(person => person.name.includes(this.state.showOnly))
     return (
       <div>
+        <h3>haku</h3>
+        <form>
+        <div>
+            rajaa: <input value={this.state.showOnly} onChange={this.handleFilter}/>
+        </div>
+        </form>
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.addName}>
           <div>
-            nimi: <input value={this.state.newName} onChange={this.handleChange}/>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
+          </div>
+          <div>
+            numero: <input value={this.state.newNumber} onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
@@ -51,7 +76,7 @@ class App extends React.Component {
         </form>
         <h2>Numerot</h2>
         <div>
-           <Numero props={this.state.persons} />
+           <Numero props={namesToShow} showOnly={this.state.showOnly} />
         </div>
       </div>
     )
