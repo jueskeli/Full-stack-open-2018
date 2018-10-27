13919f6970c046b668891fb01801e73fefd10a03
Juuso <jueskeli@student.jyu.fi>
2018-10-27 22:30:27 +0300

    Tehty tehtävät 2.18 ja 2.19

--- Section_2/puhelin_luettelo/src/App.js
+++ Section_2/puhelin_luettelo/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Numero from './components/Numero'
 import HenkiloLisays from './components/HenkiloLisays'
 import henkiloPalvelu from './services/persons'
+import Ilmoitus from './components/Ilmoitus'
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +11,8 @@ class App extends React.Component {
       persons: [],
       newName: 'anna uusi nimi',
       newNumber: '000-00000000',
-      showOnly: ''
+      showOnly: '',
+      ilmoitus : null
     }
   }
 
@@ -26,24 +28,42 @@ class App extends React.Component {
   addName = (event) => {
     event.preventDefault()
     console.log('nappia painettu')
-       const bookObject ={
-         name: this.state.newName,
-         number: this.state.newNumber
-       }
-    henkiloPalvelu
-      .create(bookObject)
-      .then(newPerson => {
-        if(this.state.persons.some(person => person.name === this.state.newName) === false &&
-        this.state.persons.some(person => person.number === this.state.newNumber) === false){ 
+
+    if(this.state.persons.some(person => person.name === this.state.newName) === false &&
+      this.state.persons.some(person => person.number === this.state.newNumber) === false){   
+      const bookObject ={
+        name: this.state.newName,
+        number: this.state.newNumber
+      }  
+      henkiloPalvelu
+        .create(bookObject)
+        .then(newPerson => {
           this.setState({
             persons : this.state.persons.concat(newPerson),
             newName : 'anna uusi nimi',
-            newNumber : '000-0000000'
+            newNumber : '000-0000000',
+            ilmoitus : `lisättiin henkilö ${bookObject.name} onnistuneesti`,
+            isError: false
+            })
+          setTimeout(() => {
+              this.setState({ilmoitus: null})
+            }, 5000)
           })
-        }
-        else alert('NIMI TAI NUMERO LÖYTYY JO OSOITEKIRJASTA')
-      })
+     }
+    else if(this.state.persons.some(person => person.number === this.state.newNumber) === false){
+      const temp = this.state.persons.filter(p=> p.name === this.state.newName)
+      console.log(temp)
+      const bookObject ={
+        id: temp[0].id,
+        name: this.state.newName,
+        number: this.state.newNumber
+      }
+          this.updateName(bookObject)
     }
+    else {
+          alert('Nimi ja numero löytyy jo osoitekirjasta')
+    }      
+  }
 
   removeName = (id) => {
       console.log('remove')
@@ -52,18 +72,53 @@ class App extends React.Component {
       if (window.confirm(`'Poistetaanko ${henkilo[0].name}`)){
        henkiloPalvelu
         .remove(id)
-        .then(
-          console.log('remove successful'),
+        .then( response => {
+          console.log('remove successful')
           this.setState({
             persons : this.state.persons.filter(person => person.id !== id),
+            ilmoitus : 'poistettiin henkilö onnistuneesti',
+            isError: false
           })
-          )
+          setTimeout(() => {
+            this.setState({ilmoitus: null})
+          }, 5000)
+        })
         .catch(error => {
-          alert(`henkiön ${id}  poisto epäonnistui`)
+          this.setState({
+            ilmoitus: `henkilö on jo valitettavasti poistettu palvelimelta`,
+            persons: this.state.persons.filter(p => p.id !== id),
+            isError: true
+          })
+        setTimeout(() => {
+            this.setState({ilmoitus: null})
+          }, 5000)
         })
       }
     }
   }
+
+  updateName = (bookObject) => {
+    console.log('update')
+    if(window.confirm(`'Päivitetäänkö ${bookObject.name}`)){
+        console.log(bookObject)
+        henkiloPalvelu
+          .update(bookObject.id, bookObject)
+          .then( response => {
+            console.log('update successful')
+            const persons = this.state.persons.filter(p => p.id !== bookObject.id)
+            this.setState({
+              persons : persons.concat(response),
+              newName : 'anna uusi nimi',
+              newNumber : '000-0000000',
+              ilmoitus : `Päivitettiin henkilön ${bookObject.name} numero onnistuneesti`,
+              isError: false
+            })
+            setTimeout(() => {
+              this.setState({ilmoitus: null})
+            }, 5000)
+      })
+  }
+}
 
   handleNameChange = (event) => {
     console.log(event.target.value)
@@ -99,6 +154,7 @@ class App extends React.Component {
           handleNumberChange={this.handleNumberChange} />
         </div>
         <h2>Numerot</h2>
+        <Ilmoitus message={this.state.ilmoitus} isError={this.state.isError}/>
         <div>
            <Numero persons={namesToShow} poista={this.removeName}/>
         </div>
