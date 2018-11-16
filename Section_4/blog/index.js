3b2a321af837e56711e84b89c95f76afd0255a4f
Juuso <jueskeli@student.jyu.fi>
2018-11-16 21:24:51 +0200

    tehty tehtävä 4.2

--- Section_4/blog/index.js
+++ Section_4/blog/index.js
@@ -4,39 +4,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const blogRouter = require('./controllers/blog')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogRouter)
 
-const mongoUrl = 'mongodb://Juuso:Lintsi12@ds145463.mlab.com:45463/section_3'
+const mongoUrl = 'mongodb://Juuso:-----@ds145463.mlab.com:45463/section_3'
 mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
